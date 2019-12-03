f037836538928b4f29e2710f0f0f850dc25cc01b
Cem KIRAY <jamcry@hotmail.com>
2019-12-03 15:58:35 +0300

    Add Request Header Parser Microservice project

---
+++ apis-and-microservices/request-header-parser/server.js
@@ -0,0 +1,41 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// parse wanted header parameters
+app.get("/api/whoami", function (req, res) {
+  const {headers} = req;
+  ipaddress = headers["x-forwarded-for"] && headers['x-forwarded-for'].split(",")[0] || headers.host || req.connection.remoteAddress;
+  res.json({
+      ipaddress: ipaddress,
+      language: headers["accept-language"],
+      software: headers["user-agent"],
+  })
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
